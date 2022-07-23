fa32f0041b2860cc1e865ebf14264f8ebd29476b
Alfredocrd <alfredocrd@hotmail.com>
2022-07-23 12:55:20 -0400

    img de home y button

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+import bg from './img/bg.jpg';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+      <img src={bg} />
+      <Link to='/breaking-bad'>
+      <Button variant="contained">Contained</Button>
+      </Link>
+    </div>
+    
   )
 }
 
